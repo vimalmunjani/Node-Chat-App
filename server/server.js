c14af0b99ad276c074dd0905904abcaff37f8a1c
Vimal Munjani <vimal.j.munjani@gmail.com>
2018-09-22 12:53:31 +0530

    Add basic client - server communication, connect & disconnect socket.io

--- server/server.js
+++ server/server.js
@@ -1,20 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const app = express();
-
-
+const socketIO = require('socket.io');
 
 const publicPath =  path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome');
-// })
+io.on('connection', (socket) => {
+    console.log(`New User connected - ${socket}`);
 
+    socket.on('disconnect', () => {
+        console.log(`User was disconnected`);
+    });
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
 })
+
+
+server.listen(port, () => {
+    console.log(`Server is runnnig on port ${port}`);
+});
 
 
