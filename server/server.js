81987e0e4a244a4d34cd56e7cafdd1b5dd85a830
Vimal Munjani <vimal.j.munjani@gmail.com>
2018-09-22 14:43:20 +0530

    Add createMessage and newMesage events for bidirectional communication

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const publicPath =  path.join(__dirname, '../public')
+const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
 
 let app = express();
@@ -13,17 +13,25 @@ let io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log(`New User connected - ${socket}`);
+    console.log(`New User connected`);
+
+    socket.on('createMessage', (message) => {
+        console.log(`new message`, message);
+    });
+
+    socket.emit('newMessage', {
+        from: '....',
+        message: 'I am emitted from server new message',
+        createdAt: 'today'
+    });
 
     socket.on('disconnect', () => {
         console.log(`User was disconnected`);
     });
 
-})
+});
 
 
 server.listen(port, () => {
     console.log(`Server is runnnig on port ${port}`);
 });
-
-
