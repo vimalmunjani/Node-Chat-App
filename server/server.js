5a732e51eb0e5bd219d0584efbd6993c1809ec66
Vimal Munjani <vimal.j.munjani@gmail.com>
2018-09-22 11:52:02 +0530

    Initialized the skeleton for the chat App

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+
+const publicPath =  path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome');
+// })
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
+
+
